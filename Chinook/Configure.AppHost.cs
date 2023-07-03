b0c7ccf4a691a8ec05fd1de5b5ba039582833910
shubuzuo <shubuzuo@gmail.com>
2023-07-03 12:21:43 +0800

    first commit

---
+++ Chinook/Configure.AppHost.cs
@@ -0,0 +1,25 @@
+using Funq;
+using ServiceStack;
+using Chinook.ServiceInterface;
+
+[assembly: HostingStartup(typeof(Chinook.AppHost))]
+
+namespace Chinook;
+
+public class AppHost : AppHostBase, IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices(services => {
+            // Configure ASP.NET Core IOC Dependencies
+        });
+
+    public AppHost() : base("Chinook", typeof(MyServices).Assembly) {}
+
+    public override void Configure(Container container)
+    {
+        // Configure ServiceStack only IOC, Config & Plugins
+        SetConfig(new HostConfig {
+            UseSameSiteCookies = true,
+        });
+    }
+}
